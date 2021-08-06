af621ab34c8877f460724187200a3096762d20c8
youcefaidoun <youcefaidoun16@gmail.com>
2021-08-06 20:38:20 +0100

    rename class Note with create def __str__ & def save for slug if null & add Note to admin

--- src/notesapp/models.py
+++ src/notesapp/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 import datetime
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 # Create your models here.
 
-class Node(models.Model):
+class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    slug = models.SlugField()
+    slug = models.SlugField(null=True,blank=True)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(blank=True, default=datetime.datetime.now)
     active = models.BooleanField(default=True)
     tags = models.CharField(max_length=100, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Note, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
